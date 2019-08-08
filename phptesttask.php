e8d9b27fae286a964f7a7c78ea4de708f4e8b3f0
Igor Vedeneev <vedeneev.ia@gmail.com>
2019-08-08 16:47:05 +0300

    first try

--- phptesttask.php
+++ phptesttask.php
@@ -1,1 +1,17 @@
-<?php echo 'PHP Test Task'; ?>
+<?php 
+/*
+Plugin Name: phptesttask
+Description: shortcode test plugin
+Version: 1.0
+*/
+echo 'PHP Test Task';
+
+function post_func($atts){
+    extract(shortcode_atts(array(
+        'value' => 'Default'
+        ), $atts));
+    return "[{$value}]"
+}
+add_shortcode('post', 'post_func');
+
+?>
