52c0534a8ffe6387a323363304bf0b568051eec4
Igor Vedeneev <vedeneev.ia@gmail.com>
2019-08-09 09:55:44 +0300

    final

--- phptesttask.php
+++ phptesttask.php
@@ -4,14 +4,26 @@ Plugin Name: phptesttask
 Description: shortcode test plugin
 Version: 1.0
 */
-echo 'PHP Test Task';
 
 function post_func($atts){
     extract(shortcode_atts(array(
         'value' => 'Default'
         ), $atts));
-    return "[{$value}]"
+
+    if(is_numeric($value)){
+        if($value%7 == 0){
+            if($value%11 == 0){
+                $value = "LongRead";    
+            } else {
+                $value = "Long";
+            }            
+        } else {
+            if ($value%11 ==0){
+                $value = "Read";
+            }
+        }
+    }
+    return "[{$value}]";
 }
 add_shortcode('post', 'post_func');
-
 ?>
